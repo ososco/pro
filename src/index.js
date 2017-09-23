e78ef564b1f1cc5f2bc41ccba47c8e6f4b1a151a
ososco <ososco@gmail.com>
2017-09-23 22:23:25 +0300

    minor update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './components/App'
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers'
@@ -9,6 +13,8 @@ const store = createStore(reducer)
 
 ReactDom.render(
 	<Provider store={store}>
-	<App />
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 	</Provider>, document.getElementById('root')
 	)
